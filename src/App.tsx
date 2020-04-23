197c54e8dda37bbc79830386e9f7bc8ce7e5e13a
taka521 <521taka.m.develop@gmail.com>
2020-04-23 20:50:08 +0900

    apollo-boost -> apollo-client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {Users} from './components/Users';
+import { Users } from './components/Users';
 
 const App = () => {
-  return <div className="App"><Users /></div>;
+  return <div className="App"><Users/></div>;
 };
 
 export default App;
--- src/config/apolloClient.ts
+++ src/config/apolloClient.ts
@@ -1,9 +1,39 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { setContext } from "apollo-link-context";
+import { ApolloLink } from "apollo-link";
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${ message }, Location: ${ locations }, Path: ${ path }`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${ networkError }`);
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = '73a6b409-6aab-42b8-9f88-c3f3879b9870';
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${ token }` : "",
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:9002/graphql'
+})
 
 const cache = new InMemoryCache();
+
 const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
   cache,
-  uri: 'http://localhost:9002/graphql',
 });
 
 export { client };
